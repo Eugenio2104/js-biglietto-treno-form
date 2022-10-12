6102db017d7acb32e136e70ae87e8db7d4ef377a
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2022-10-12 19:01:05 +0200

    prova

--- js/script.js
+++ js/script.js
@@ -1,47 +1,47 @@
 const inputNome = document.getElementById('inputNome');
-const inputKm = document.getElementById('inputKm');
+const inputKm = document.getElementById ('inputKm');
 const inputEta = document.getElementById('inputEta')
 const prezzoAlKm = 0.21;
+const prezzoBiglietto = (inputKm * prezzoAlKm)
 let sconto = 0;
-let scontoApplicato;
-let prezzoScontato;
-let prezzoFinale;
+let prezzoFinale = document.getElementById (`inputPrezzo`);
+let etaPasseggero = document.getElementById(`eta`)
 const bottoneGenera = document.getElementById('btn-input');
 
 bottoneGenera.addEventListener('click', function() {
   const nome = inputNome.value;
   const km = inputKm.value;
   const eta = inputEta.value; 
+  let prezzoFinale  = eta.value;
  // Parte Logica
 
   document.getElementById(`output-name`).innerHTML = nome
 
   
   
-  prezzoFinale = prezzoBiglietto;
-if(etaPasseggero < 18){
+  
+if(etaPasseggero ===`minorenne`){
   sconto = 0.2;
-  literalAge = (`minorenne`)
   prezzoFinale = prezzoBiglietto - (prezzoBiglietto * sconto);
 }
 
-else if(etaPasseggero > 65) {
+else if(etaPasseggero === `over65`){
   sconto = 0.4;
-  literalAge = (`over65`)
     prezzoFinale = prezzoBiglietto - (prezzoBiglietto * sconto);
 }
 
-else{
-  literalAge = (`maggiorenne`)
-}
+else(etaPasseggero === `maggiorenne`)
 
-document.getElementById(`prezzo-biglietto`).innerHTML = prezzoFinale; 
+document.getElementById(`inputPrezzo`).innerHTML = `
+${prezzoFinale}
+`; 
  
  // Parte di presentazione
 
 console.log('Nome ', nome);
 console.log('KM ', km);
 console.log('ETA', eta);
+console.log(`PREZZO`, )
 })
 
 
