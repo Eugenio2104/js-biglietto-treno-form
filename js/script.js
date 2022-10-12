832487b58aa73313029889a2117a8033c12bf877
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2022-10-12 16:23:54 +0200

    push prova

--- js/script.js
+++ js/script.js
@@ -1,12 +1,32 @@
+const nomeCognome = ("Inserisci nome e cognome");
+const kmDaPercorrere = parseInt("Quanti km vuoi percorrere?");
+const etaPasseggero = parseInt("Quanti anni hai?"); 
+const prezzoKm = 0.21;
+const prezzoBiglietto = (kmDaPercorrere * prezzoKm)
+let sconto = 0;
+let scontoApplicato;
+let prezzoScontato;
+let prezzoFinale;
+let output;
 
+prezzoFinale = prezzoBiglietto;
 
+if(etaPasseggero < 18){
+  sconto = 0.2;
+  literalAge = (`minorenne`)
+  prezzoFinale = prezzoBiglietto - (prezzoBiglietto * sconto);
+}
 
-const btnInput = document.getElementById('btn-input');
-btnInput.addEventListener('click', function(){
-  const name = document.getElementById('input').value;
-  //alert('Nell\'input c\'è scritto: ' + name);
-  document.getElementById('output-name').innerHTML = name;
+else if(etaPasseggero > 65) {
+  sconto = 0.4;
+  literalAge = (`over65`)
+    prezzoFinale = prezzoBiglietto - (prezzoBiglietto * sconto);
+}
 
-  //resetto i lvalue dell'input
-  document.getElementById('input').value = '';
-});
+else{
+  literalAge = (`maggiorenne`)
+}
+
+console.log('Il prezzo del biglietto è ',prezzoFinale)
+
+document.getElementById(`prezzo-biglietto`).innerHTML += output;
