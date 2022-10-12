574a5b0945051f99c128b7a8f83ecfea272f2c99
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2022-10-12 17:22:21 +0200

    prova

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
+const inputNome = document.getElementById('inputNome');
+const inputKm = document.getElementById('inputKm');
+const inputEta = document.getElementById('inputEta')
+const bottoneGenera = document.getElementById('btn-input');
+
+bottoneGenera.addEventListener('click', function() {
+ const nome = inputNome.value;
+ const km = inputKm.value;
+ const eta = inputEta.value;
+
+ // Parte Logica
+  
+  document.getElementById(`output-name`).innerHTML = literalAge;
+  document.getElementById(`prezzo-biglietto`).innerHTML = prezzoFinale;
+
+ // Parte di presentazione
+
+console.log('Nome ', nome);
+console.log('KM ', km);
+console.log('ETA', eta);
+})
+
+
+
+
+
+
 const nomeCognome = ("Inserisci nome e cognome");
 const kmDaPercorrere = parseInt("Quanti km vuoi percorrere?");
 const etaPasseggero = parseInt("Quanti anni hai?"); 
